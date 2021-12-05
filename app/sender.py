b0b6e39f82295be58f2f193b4adb485178b12a00
Marcus Davi Argolo Ribeiro <marcusdavi@gmail.com>
2021-12-05 11:32:24 -0300

    redes

--- app/sender.py
+++ app/sender.py
@@ -1,10 +1,28 @@
+import psycopg2
 from bottle import route, run, request
+
+DSN = 'dbname=email_sender user=postgres host=db'
+SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+
+def register_message(assunto, mensagem):
+  connection = psycopg2.connect(DSN)
+  cursor = connection.cursor()
+  cursor.execute(SQL, (assunto, mensagem))
+  connection.commit()
+  cursor.close()
+  connection.close()
+
+  print ('Mensagem registrada!')
+
 
 @route('/', method='POST')
 def send():
   assunto = request.forms.get('assunto')
   mensagem = request.forms.get('mensagem')
-  return 'Mensagem enfileirada! Assunto: {} Mensagem {}'.format(
+
+  register_message(assunto, mensagem)
+
+  return 'Mensagem enfileirada! Assunto: {}, Mensagem {}'.format(
     assunto, mensagem
   )
 
